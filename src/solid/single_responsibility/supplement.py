76c70e030624e097caa35c590674d341067ab059
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-01-25 12:34:05 +0000

    Enhance initial example

--- src/solid/single_responsibility/supplement.py
+++ src/solid/single_responsibility/supplement.py
@@ -1,8 +1,18 @@
 class MP3File:
+    mp3_data: bytes
+
+    def __init__(self, data: bytes):
+        self.mp3_data = data
+
     def stream_mp3_data(self):
-        ...
+        return self.mp3_data
 
 
 class WAVFile:
+    wav_data: bytes
+
+    def __init__(self, data: bytes):
+        self.wav_data = data
+
     def stream_wav_data(self):
-        ...
+        return self.wav_data
