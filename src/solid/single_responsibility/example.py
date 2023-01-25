9bb043bce06a78f7d2c22e48fd7010e6c2550007
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-01-25 12:25:22 +0000

    Add initial example

---
+++ src/solid/single_responsibility/example.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+
+from src.solid.single_responsibility.supplement import MP3File
+
+
+# anti-pattern
+class BadSoundSpeaker:
+    speaker_id: UUID
+    volume: int
+    powered_on: bool
+
+    def power_on(self):
+        ...
+
+    def power_off(self):
+        ...
+
+    def change_volume(self, new_volume: int):
+        ...
+
+    def play_music(self, music_file: MP3File):
+        if self.powered_on:
+            return music_file.stream_mp3_data()
+        else:
+            return None
---
+++ src/solid/single_responsibility/supplement.py
@@ -0,0 +1,8 @@
+class MP3File:
+    def stream_mp3_data(self):
+        ...
+
+
+class WAVFile:
+    def stream_wav_data(self):
+        ...
