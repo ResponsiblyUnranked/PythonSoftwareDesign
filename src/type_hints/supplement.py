987bc4dd869ce3159cdb1048764c973cb107ab46
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-03-02 15:50:27 +0000

    Add basic implementation for type-hint and tests

--- src/type_hints/supplement.py
+++ src/type_hints/supplement.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from typing import Any
 
 
 @dataclass
 class User:
-    ...
+    db_details: Any
 
 
 class Database:
     def create_item(self, *args, **kwargs) -> User:
-        ...
+        return User(self)
