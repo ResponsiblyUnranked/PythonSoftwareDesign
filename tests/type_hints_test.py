9d0fab627d8de02ef05baa56655c5512d7b802f5
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-03-02 15:50:41 +0000

    Add single test to pass checks

---
+++ tests/type_hints_test.py
@@ -0,0 +1,18 @@
+import datetime
+import uuid
+
+from src.type_hints.examples import add_user
+from src.type_hints.supplement import User
+
+
+def test_example_returns_correct_type() -> None:
+    # given
+    user_id = uuid.uuid4()
+    date_of_birth = datetime.date(2000, 1, 1)
+    username = "foo"
+
+    # when
+    response = add_user(user_id, date_of_birth, username)
+
+    # then
+    assert isinstance(response, User)
