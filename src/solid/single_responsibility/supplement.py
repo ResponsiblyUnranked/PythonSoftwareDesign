37e27033464fa53de769c17c3e5d8b07db74bc2c
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-03-03 21:57:49 +0000

    Add 'Sound' class to speaker output

--- src/solid/single_responsibility/supplement.py
+++ src/solid/single_responsibility/supplement.py
@@ -1,11 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Sound:
+    sound_data: bytes
+
+
 class MP3File:
     mp3_data: bytes
 
     def __init__(self, data: bytes):
         self.mp3_data = data
 
-    def stream_mp3_data(self):
-        return self.mp3_data
+    def stream_mp3_data(self) -> Sound:
+        return Sound(self.mp3_data)
 
 
 class WAVFile:
@@ -14,5 +22,5 @@ class WAVFile:
     def __init__(self, data: bytes):
         self.wav_data = data
 
-    def stream_wav_data(self):
-        return self.wav_data
+    def stream_wav_data(self) -> Sound:
+        return Sound(self.wav_data)
