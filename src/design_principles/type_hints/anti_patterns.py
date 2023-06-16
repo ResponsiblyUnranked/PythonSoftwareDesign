c13325592c938080e3bcf19c925eb4694cdc839c
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-06-16 19:03:17 +0100

    Combine anti-pattern into example file

--- src/design_principles/type_hints/anti_patterns.py
+++
@@ -1,9 +0,0 @@
-from src.design_principles.type_hints.supplement import Database
-
-
-# anti-pattern 1
-def add_user(user, dob, name):
-    database = Database()
-    result = database.create_item(user, dob, name)
-
-    return result
