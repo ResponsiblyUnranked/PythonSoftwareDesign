a4a893a5ac74644d23a0baf8697c301bccc281d9
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-06-16 19:03:17 +0100

    Combine anti-pattern into example file

--- src/design_principles/type_hints/example.py
+++ src/design_principles/type_hints/example.py
@@ -4,6 +4,14 @@ from uuid import UUID
 from src.design_principles.type_hints.supplement import Database, User
 
 
+# anti-pattern
+def bad_add_user(user, dob, name):
+    database = Database()
+    result = database.create_item(user, dob, name)
+
+    return result
+
+
 # example 1
 def add_user(user: UUID, dob: datetime.date, name: str) -> User:
     database = Database()
