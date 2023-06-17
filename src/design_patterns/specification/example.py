636bd79d7f2bd9feb082779cd0bafe234de9f559
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-06-17 07:38:21 +0100

    Complete anti-pattern for specification pattern

--- src/design_patterns/specification/example.py
+++ src/design_patterns/specification/example.py
@@ -1,15 +1,26 @@
-from src.design_patterns.specification.supplement import InvalidEmployeeError, \
-    Department
+from typing import List
+
+from src.design_patterns.specification.supplement import (
+    Department,
+    InvalidEmployeeError,
+)
 
 
 class Employee:
     def __init__(
-        self, name: str, age: int, department: Department, years_worked: int = 0
+        self,
+        name: str,
+        age: int,
+        department: Department,
+        salary: int = 30_000,
+        years_worked: int = 0,
     ) -> None:
         self.name = name
         self.age = age
         self.department = department
+        self.salary = salary
         self.years_worked = years_worked
+        self.previous_bonus_years: List[str] = []
 
     def save(self) -> None:
         pass
@@ -30,12 +41,32 @@ def hire_new_employee(name: str, age: int, department: Department) -> Employee:
     return new_employee
 
 
-def give_employee_raise(employee: Employee) -> Employee:
+def is_employee_eligible_for_a_raise(employee: Employee) -> bool:
     if employee.age < 18:
-        return employee
+        return False
 
     if employee.department == Department.SALES:
-        # TODO: finish this
-        pass
+        if employee.salary < 10_000:
+            return False
 
-    return employee
+        if employee.salary >= 10_000:
+            if employee.age >= 75:
+                return False
+            else:
+                return True
+
+    elif employee.department == Department.FINANCE:
+        if employee.salary > 85_000:
+            return False
+        return True
+
+    elif employee.department == Department.DEVELOPMENT:
+        return True
+
+    elif employee.department == Department.HR:
+        for year in employee.previous_bonus_years:
+            if year == "2022":
+                return False
+        return True
+
+    return False
