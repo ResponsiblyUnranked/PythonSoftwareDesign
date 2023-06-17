df2c4009c5453c517e7a82f572f2325940976841
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-06-17 08:14:26 +0100

    Add new tests for employee raise eligibility

--- src/design_patterns/specification/tests/fixtures.py
+++ src/design_patterns/specification/tests/fixtures.py
@@ -1,3 +1,4 @@
+from src.design_patterns.specification.example import Employee
 from src.design_patterns.specification.supplement import Department
 
 valid_new_employees = [
@@ -11,4 +12,15 @@ invalid_new_employees = [
     ("Clare", 28, Department.SALES),
     ("Sarah", 102, Department.MARKETING),
     ("", 45, Department.HR),
+]
+
+employees_getting_a_raise = [
+    Employee("Iroh", 70, Department.SALES, salary=25_000),
+    Employee("Azula", 25, Department.SALES, salary=32_000),
+]
+
+employees_not_getting_a_raise = [
+    Employee("Momo", 12, Department.HR),
+    Employee("Zuko", 22, Department.SALES, salary=8_000),
+    Employee("Roku", 98, Department.SALES, salary=32_000),
 ]
