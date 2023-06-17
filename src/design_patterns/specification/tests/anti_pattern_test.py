d414ce30c5c18122ef18b1266ba128ed05c7288e
Jamie-McKernan <jamie.mckernan@gmail.com>
2023-06-17 08:14:26 +0100

    Add new tests for employee raise eligibility

--- src/design_patterns/specification/tests/anti_pattern_test.py
+++ src/design_patterns/specification/tests/anti_pattern_test.py
@@ -1,13 +1,14 @@
 import pytest
 
-from src.design_patterns.specification.example import Employee, hire_new_employee
+from src.design_patterns.specification.example import Employee, hire_new_employee, \
+    is_employee_eligible_for_a_raise
 from src.design_patterns.specification.supplement import (
     Department,
     InvalidEmployeeError,
 )
 from src.design_patterns.specification.tests.fixtures import (
     invalid_new_employees,
-    valid_new_employees,
+    valid_new_employees, employees_getting_a_raise, employees_not_getting_a_raise,
 )
 
 
@@ -28,3 +29,21 @@ def test_error_raised_for_invalid_employee(
     with pytest.raises(InvalidEmployeeError):
         # when
         hire_new_employee(name, age, department)
+
+
+@pytest.mark.parametrize("employee", employees_getting_a_raise)
+def test_employee_is_eligible_for_a_raise(employee: Employee) -> None:
+    # when
+    is_getting_raise = is_employee_eligible_for_a_raise(employee)
+
+    # then
+    assert is_getting_raise
+
+
+@pytest.mark.parametrize("employee", employees_not_getting_a_raise)
+def test_employee_is_not_eligible_for_a_raise(employee: Employee) -> None:
+    # when
+    is_getting_raise = is_employee_eligible_for_a_raise(employee)
+
+    # then
+    assert not is_getting_raise
