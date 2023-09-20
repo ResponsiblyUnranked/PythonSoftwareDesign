6812970dcd56a269b93a2ed4e4f8ffd97cc36ad3
Jamie McKernan <jamie.mckernan@gmail.com>
2023-09-20 14:02:10 +0100

    Add basic example of a happy pattern

---
+++ src/design_principles/solid/liskov/tests/weaken_postconditions_test.py
@@ -0,0 +1,47 @@
+from src.design_principles.solid.liskov.detailed.weaken_postconditions import (
+    DatabaseConnection,
+)
+
+
+def test_can_create_database_connection() -> None:
+    # when
+    connection = DatabaseConnection("192.168.0.24:25565")
+
+    # then
+    assert isinstance(connection, DatabaseConnection)
+    assert not connection.is_open
+
+
+def test_can_connect_to_database() -> None:
+    # given
+    connection = DatabaseConnection("192.168.0.24:25565")
+
+    # when
+    connection._open()
+
+    # then
+    assert connection.is_open
+
+
+def test_connection_can_be_closed() -> None:
+    # given
+    connection = DatabaseConnection("192.168.0.24:25565")
+    connection._open()
+
+    # when
+    connection._close()
+
+    # then
+    assert not connection.is_open
+
+
+def test_connection_is_closed_after_query() -> None:
+    # given
+    connection = DatabaseConnection("192.168.0.24:25565")
+
+    # when
+    result = connection.query("SELECT * FROM my_data;")
+
+    # then
+    assert result
+    assert not connection.is_open
