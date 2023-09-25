be29986e456fd4fc4b1710bfc765a2eacb85975e
Jamie McKernan <jamie.mckernan@gmail.com>
2023-09-25 14:29:22 +0100

    Add tests

---
+++ src/design_patterns/command/tests/example_one_test.py
@@ -0,0 +1,37 @@
+from src.design_patterns.command.example_one import (
+    KeyboardHandler,
+    PhotoshopToolSelector,
+    SelectBrush,
+    SelectEraser,
+)
+
+
+def test_can_use_commands() -> None:
+    # given
+    tool_selector = PhotoshopToolSelector()
+
+    key_bindings = {
+        "b": SelectBrush(tool_selector),
+        "e": SelectEraser(tool_selector),
+    }
+
+    key_handler = KeyboardHandler(key_bindings)
+
+    # when
+    key_handler.handle_input("b")
+    key_handler.handle_input("e")
+
+
+def test_unbound_keys_do_not_raise_error() -> None:
+    # given
+    tool_selector = PhotoshopToolSelector()
+
+    key_bindings = {
+        "b": SelectBrush(tool_selector),
+        "e": SelectEraser(tool_selector),
+    }
+
+    key_handler = KeyboardHandler(key_bindings)
+
+    # when
+    key_handler.handle_input("x")
