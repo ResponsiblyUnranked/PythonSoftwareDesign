366c11ac348124ebe0c1aee3a3b25acbdef8bd46
Jamie McKernan <jamie.mckernan@gmail.com>
2023-10-25 14:44:31 +0100

    Add test to demonstrate game code

---
+++ src/design_patterns/command/tests/game_example_test.py
@@ -0,0 +1,19 @@
+from src.design_patterns.command.game_example import GameEngine, MoveCommand, LandUnit, SeaUnit, DestroyCommand
+from src.design_patterns.command.supplement import MovementDirection
+
+
+def test_player_can_take_turn() -> None:
+    # given
+    engine = GameEngine()
+    land_unit = LandUnit()
+    sea_unit = SeaUnit()
+
+    first_move = MoveCommand(land_unit, MovementDirection.NORTH, 2)
+    second_move = MoveCommand(land_unit, MovementDirection.EAST, 5)
+    third_move = MoveCommand(sea_unit, MovementDirection.SOUTH, 6)
+    fourth_move = DestroyCommand(land_unit)
+
+    engine.queue_commands(first_move, second_move, third_move, fourth_move)
+
+    # when
+    engine.execute_turn()
